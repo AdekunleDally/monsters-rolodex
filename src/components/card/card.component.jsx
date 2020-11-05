764de9fcef03a83f3585c5e93abbbd5acc32e37b
AdekunleDally <adekunledally@gmail.com>
2020-11-04 18:47:14 -0800

    adding monsters-rolodex files

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card =(props)=>(
+    <div className="card-container">
+      <img src={`http://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt='monster' />
+      <h2>{props.monster.name}</h2> 
+      <p>{props.monster.email}</p>
+    </div>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import './card.styles.css';
+// export const Card=(props)=>(
+//     <div className="card-container">
+//       <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+//     <h2>{props.monster.name}</h2> 
+//     <p>{props.monster.email}</p>
+//     </div>
+// )
